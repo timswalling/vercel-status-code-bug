765f00333f2800a12914e1a205b3670307453623
Tim Swalling <tim@paws.com>
2020-06-16 12:11:47 +0100

    Add error and response status codes to error page

--- pages/_error.js
+++ pages/_error.js
@@ -1,18 +1,30 @@
 import React from "react";
 
-function Error({ statusCode }) {
+function Error({ errStatusCode, resStatusCode, statusCode }) {
   return (
-    <p>
-      {statusCode
-        ? `An error ${statusCode} occurred on server`
-        : "An error occurred on client"}
-    </p>
+    <main>
+      <p>
+        {statusCode
+          ? `An error ${statusCode} occurred on server`
+          : "An error occurred on client"}
+      </p>
+      <dl>
+        <dt>Error status code</dt>
+        <dd>{errStatusCode || "Not set"}</dd>
+        <dt>Response status code</dt>
+        <dd>{resStatusCode || "Not set"}</dd>
+      </dl>
+    </main>
   );
 }
 
 Error.getInitialProps = ({ res, err }) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-  return { statusCode };
+
+  const errStatusCode = err && err.statusCode;
+  const resStatusCode = res && res.statusCode;
+
+  return { errStatusCode, resStatusCode, statusCode };
 };
 
 export default Error;
